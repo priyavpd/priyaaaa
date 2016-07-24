88353920aeee3dd462b1928598c6463afbcb59d1
priyavpd <vpriyacse2@gmail.com>
2016-07-24 12:03:13 +0530

    Create Stringint.java

---
+++ Stringint.java
@@ -0,0 +1,14 @@
+class Strigint {
+
+public static void main(String[] args) {
+    String s="1313088";
+    String s1="88";
+	int x =Integer.parseInt(s);
+  
+    int y = Integer.parseInt(s1);
+
+    System.out.println(x);
+    System.out.println(y);
+}
+
+}
